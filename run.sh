2079c5195371b0b2423434ccebe51511022af175
Bruce Szalwinski <szalwinb@adp.com>
2013-11-12 17:31:42 -0600

    change path to environment variable

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -cp /Users/szalwinb/groovy-2.1.9/embeddable/groovy-all-2.1.9.jar:target/gameoflife-1.0-SNAPSHOT.jar gameoflife.GameOfLife
+mvn install
+java -cp $GROOVY_HOME/embeddable/groovy-all-2.1.9.jar:target/gameoflife-1.0-SNAPSHOT.jar gameoflife.GameOfLife
